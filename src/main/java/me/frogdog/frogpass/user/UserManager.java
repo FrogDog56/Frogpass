023aa31426fe09f85f79d0be0179cab4d0dc26cf
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-04-01 00:07:29 +0100

    fixed saving username

--- src/main/java/me/frogdog/frogpass/user/UserManager.java
+++ src/main/java/me/frogdog/frogpass/user/UserManager.java
@@ -1,11 +1,11 @@
 package me.frogdog.frogpass.user;
 
+import com.google.gson.stream.JsonWriter;
 import me.frogdog.frogpass.Main;
 import me.frogdog.frogpass.data.Data;
 import me.frogdog.frogpass.util.Registry;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public final class UserManager extends Registry<User> {
@@ -28,6 +28,31 @@ public final class UserManager extends Registry<User> {
 
             @Override
             public void save() {
+                try {
+                    if (!this.getFile().exists()) {
+                        this.getFile().createNewFile();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    JsonWriter writer = new JsonWriter(new FileWriter(this.getFile()));
+                    writer.beginObject();
+                    writer.name("data");
+                    writer.beginArray();
+                    for (User u : Main.getInstance().getUserManager().getRegistry()) {
+                        writer.beginObject();
+                        writer.name("username").value(u.getUsername());
+                        writer.name("password").value(u.getPassword());
+                        writer.endObject();
+                    }
+                    writer.endArray();
+                    writer.endObject();
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         };
